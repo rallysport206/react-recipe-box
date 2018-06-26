d746194c050d30e8e19e8a80c106ef0a8a37ff7e
rallysport206 <phan.tony125@gmail.com>
2018-06-26 16:49:01 -0700

    data recipe manipulate and call function

--- src/addrecipe.js
+++ src/addrecipe.js
@@ -24,6 +24,40 @@ export class AddRecipe extends React.Component {
         var nawIngredients = this.state.ingredients.split(regExp);
         var newRecipe = {name: newName, ingredients: newIngredients};
         onAdd(newRecipe);
-        
+        this.setState({name:"", ingredients: ""});
     }
-}
+    handleCancel() {
+        const onAddModal = this.props.onAddModal;
+        this.setState({name: "", ingredients: ""});
+        onAddModal();
+    }
+    render(){
+        const onShow = this.props.onShow;
+        var regex1 = /^\S/;
+        var regex2 = /^[^,\s]/;
+        var regex3 = /[^,\s]$/;
+        const validRecipe = regex1.test(this.state.name) &&
+    regex2.test(this.state.ingredients) &&
+    regex3.test(this.state.ingredients);
+        return(
+            <Modal show{onShow} onHide={this.handleCancel}>
+                <Modal.Header closeButton>
+                    <Modal.Title>New Recipe</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <FormGroup controlId="formControlsName">
+                        <ControlLabel>Recipe Name</ControlLabel>
+                        <FormControl type="text" require onChange={this.handleRecipeNameChange} value={this.state.name} placeholder="Enter Name" />
+                    </FormGroup>
+                    <FormGroup controlId="formControlsIngredients">
+                        <ControlLabel>Recipe Ingredients</ControlLabel>
+                        <FormControl componentClass="textarea" type="text" require onChange={this.handleRecipeIngredientsChange} value={this.state.ingredients} placeholder="Enter Ingredients (separate by commas)" />
+                    </FormGroup>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button disabled={!validRecipe} bsStyle="success" onClick={this.handleSubmit}>Save Recipe</Button>
+                </Modal.Footer>    
+            </Modal>
+        );
+    }
+};
